70b9f28a9128e79910b5dea08bfc8d5085a6d81f
DAlav <D.Alavaro@gmail.com>
2023-11-18 23:13:30 +0300

    20.2

--- dogs/views.py
+++ dogs/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+from dogs.models import Category
+
+
+def index(request):
+    context = {
+        'objects_list': Category.objects.all()[0:3],
+        'title': 'Главная страница'
+    }
+    return render(request, 'dogs/index.html', context)
+
+
+def categories(request):
+    context = {
+        'objects_list': Category.objects.all(),
+        'title': 'Питомник - Все наши породы'
+    }
+    return render(request, 'dogs/categories.html', context)
